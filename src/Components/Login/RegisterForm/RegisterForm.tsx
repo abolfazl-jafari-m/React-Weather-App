db2434570aa72f30e8b156feeebfc559be9e0250
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-02-28 20:50:41 +0330

    feat(LoginForm): change Vaildation using react-form-hook

--- src/Components/Login/RegisterForm/RegisterForm.tsx
+++ src/Components/Login/RegisterForm/RegisterForm.tsx
@@ -1,91 +1,66 @@
-import Input from "../../Base/Input/Input.tsx";
+// import Input from "../../Base/Input/Input.tsx";
 import {motion} from "motion/react";
-import {FormEvent, useContext, useState} from "react";
+import {useContext, useState} from "react";
 import {LoginContext, LoginContextInterface} from "../../../Context/LoginContext.tsx";
 import {REGEX_EMAIL, REGEX_PASSWORD} from "../../../Constants/Constants.ts";
-import {register} from "../../../Services/User.ts";
+import {register as registerUser} from "../../../Services/User.ts";
 import useLocalStorage from "../../../Hooks/useLocalStorage/useLocalStorage.ts";
 import {useNavigate} from "react-router";
 import toast from "react-hot-toast";
 import {FaEye} from "react-icons/fa";
 import {FiEyeOff} from "react-icons/fi";
+import {SubmitHandler, useForm} from "react-hook-form";
 
+type RegsterFormType = {
+    email: string,
+    password: string,
+    rePassword: string
+}
 
 function RegisterForm() {
     const {setIsLogin} = useContext(LoginContext) as LoginContextInterface
-    const [errors, setErrors] = useState({email: "", password: "", rePassword: ""});
     const [, setToken] = useLocalStorage("token", []);
     const navigate = useNavigate();
     const [showPass, setShowPass] = useState<boolean>(false);
+    const {handleSubmit, register, formState: {errors} , getValues} = useForm<RegsterFormType>();
 
 
-    const validation = (email: string, password: string, rePassword: string) => {
-        const newErrors = {email: "", password: "", rePassword: ""}
-        if (email === "") {
-            newErrors.email = "Please Enter Your Email"
-        } else if (!REGEX_EMAIL.test(email)) {
-            newErrors.email = "Please Enter a Valid Email"
-        }
-
-        if (password === "") {
-            newErrors.password = "Please Enter a Password"
-        } else if (!REGEX_PASSWORD.test(password)) {
-            newErrors.password = "Your Password have to at least 8  at least 8 characters & One digits & Special characters"
-        } else if (password !== rePassword) {
-            newErrors.password = "Passwords Not the Same"
-        }
-
-        if (rePassword === "") {
-            newErrors.rePassword = "Please Enter a Password"
-        } else if (!REGEX_PASSWORD.test(rePassword)) {
-            newErrors.rePassword = "Your Password have to at least 8  at least 8 characters & One digits & Special characters"
-        } else if (rePassword !== password) {
-            newErrors.rePassword = "Passwords Not the Same"
-        }
-
-        setErrors(newErrors);
-
-        return newErrors;
-    }
-
-
-    const handleRegister = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const {email, password, rePassword} = e.target as HTMLFormElement;
-        const errors = validation(email.value, password.value, rePassword.value);
-        if (Object.values(errors).every(item => item === "")) {
-            const loadingToast = toast.loading("loading...", {
-                style: {width: '320px'}
+    const handleRegister: SubmitHandler<RegsterFormType> = (data) => {
+        const loadingToast = toast.loading("loading...", {
+            style: {width: '320px'}
+        })
+        registerUser(data.email, data.password).then((res) => {
+            setToken(res.accessToken)
+            toast.success("Register was Successful", {
+                id: loadingToast
             })
-            register(email.value, password.value).then((res) => {
-                setToken(res.accessToken)
-                toast.success("Register was Successful", {
-                    id: loadingToast
-                })
-                navigate("/");
-            }).finally(() => {
-                toast.remove(loadingToast)
-            })
-        }
-
-
+            navigate("/");
+        }).finally(() => {
+            toast.remove(loadingToast)
+        })
     }
 
     return (
         <div
             className={"flex flex-col gap-10 items-center max-lg:w-full max-lg:h-full max-lg:rounded-none w-2/3 bg-gray-950/70 backdrop-blur-xl backdrop-brightness-50 rounded-lg shadow-black shadow-lg  p-5 font-fuzzy "}>
             <h2 className={"font-bold text-4xl text-white"}>Register</h2>
-            <form onSubmit={(e: FormEvent<HTMLFormElement>) => handleRegister(e)}
+            <form onSubmit={handleSubmit(handleRegister)}
                   className={"w-full flex flex-col gap-7"}>
                 <div className={"flex flex-col gap-2"}>
                     <div
                         className={"flex flex-col gap-2 bg-zinc-900/25 has-focus:bg-zinc-950/40 text-white border-2 border-gray-800/50 transition ease-linear duration-200 p-2 has-focus:border-black rounded-md"}>
                         <label className={"text-lg uppercase"}>email :</label>
-                        <Input type={"text"} className={"bg-transparent py-1 px-3 rounded-md outline-none"}
-                               placeholder={"Please Enter Your Name"} name={"email"}/>
+                        <input type={"text"} className={"bg-transparent py-1 px-3 rounded-md outline-none"}
+                               placeholder={"Please Enter Your Name"} {...register("email", {
+                            required: "this Field is Required",
+                            pattern: {
+                                value: REGEX_EMAIL,
+                                message: "Your Email is Invalid"
+                            }
+                        })}/>
                     </div>
                     <div className={"text-xs text-red-400 tracking-tighter px-2 min-h-6"}>
-                        {errors.email}
+                        {errors.email && <span>{errors.email.message}</span>}
                     </div>
                 </div>
 
@@ -94,9 +69,19 @@ function RegisterForm() {
                         className={"flex flex-col gap-2 bg-zinc-900/25 has-focus:bg-zinc-950/40 text-white border-2 border-gray-800/50 transition ease-linear duration-200 p-2 has-focus:border-black rounded-md"}>
                         <label className={"text-lg uppercase"}>Password :</label>
                         <div className={"flex items-center w-full"}>
-                            <Input type={!showPass ? "password" : "text"}
+                            <input type={!showPass ? "password" : "text"}
                                    className={"bg-transparent flex-1 py-1 px-3 rounded-md outline-none"}
-                                   placeholder={"Your Password..."} name={"password"}/>
+                                   placeholder={"Your Password..."} {...register("password", {
+                                required: "this Field is Required",
+                                pattern: {
+                                    value: REGEX_PASSWORD,
+                                    message: "Your password have to Contains 8 character & digits & Special Letter"
+                                },
+                                validate : (data)=>{
+                                    if (data !== getValues("rePassword"))
+                                        return "password must be the Same"
+                                }
+                            })}/>
                             <div onClick={() => setShowPass(!showPass)} className={"px-2"}>
                                 {
                                     !showPass ? <FaEye/> : <FiEyeOff className={"text-white"}/>
@@ -105,7 +90,8 @@ function RegisterForm() {
                         </div>
                     </div>
                     <div className={"text-xs text-red-400 tracking-tighter px-2 min-h-6"}>
-                        {errors.password}
+                        {errors.password && <span>{errors.password.message}</span>}
+
                     </div>
                 </div>
 
@@ -113,12 +99,24 @@ function RegisterForm() {
                     <div
                         className={"flex flex-col gap-2 bg-zinc-900/25 has-focus:bg-zinc-950/40 text-white border-2 border-gray-800/50 transition ease-linear duration-200 p-2 has-focus:border-black rounded-md"}>
                         <label className={"text-lg uppercase"}>Repeat Password :</label>
-                        <Input type={!showPass ? "password" : "text"}
+                        <input type={!showPass ? "password" : "text"}
                                className={"bg-transparent py-1 px-3 rounded-md outline-none"}
-                               placeholder={"Repeat Your Password..."} name={"rePassword"}/>
+                               placeholder={"Repeat Your Password..."} {...register("rePassword", {
+                                required: "this Field is Required",
+                                pattern: {
+                                    value: REGEX_PASSWORD,
+                                    message: "Your password have to Contains 8 character & digits & Special Letter"
+                                },
+                                validate : (data)=>{
+                                    if (data !== getValues("password")){
+                                        return "password must be the Same"
+                                    }
+                                }
+                            }
+                        )}/>
                     </div>
                     <div className={"text-xs text-red-400 tracking-tighter px-2 min-h-6"}>
-                        {errors.rePassword}
+                        {errors.rePassword && <span>{errors.rePassword.message}</span>}
                     </div>
                 </div>
 
