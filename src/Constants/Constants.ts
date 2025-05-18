a19467d3a5c6b641eca3e23bdb8967c894164338
AbolfazlJM <abolfazljafari563@gmail.com>
2025-05-18 18:54:18 +0330

    fix :  change user base url api

--- src/Constants/Constants.ts
+++ src/Constants/Constants.ts
@@ -1,6 +1,6 @@
 export const Weather_API_KEY = "311f6d3ecae9dc3f824552ce8cb3a67c";
 
-export const USER_BASE_URL = "http://api.alikooshesh.ir:3000/api/users";
+export const USER_BASE_URL = "https://viva-back-end.onrender.com/api/users";
 export const API_KEY = "Jafari-M-124jc4mpkFur7lP4J0WqFJj0hw8sNzKr6yZa5RwkiBsivLPcTsTvUeUkWfyB9xInMPoH0lGppvmHskpCc4qbbEeth6ix3alIWanmDywUK9TjQMKq3hhdwhg";
 
 export const REGEX_EMAIL = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/
